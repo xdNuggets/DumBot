851c42f451a43619dfbbd4902604d20b5545db51
Mad <68274411+Mad8867@users.noreply.github.com>
2020-07-26 20:54:29 +0430

    don't try this at home kids

--- cogs/annoying.py
+++ cogs/annoying.py
@@ -1,21 +1,24 @@
 import discord
 import re
 from discord.ext import commands
-
-class Antilink():
+"""
+this cog is good for some servers but in general you might wanna
+drop some memes and yeah have fun
+"""
+class Antilink(): # it all makes sense now
     def __init__(self, bot):
         self.bot = bot
-        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
+        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+') # is a link? 
         
     async def delete_link(self, message):
-        if not message.channel:
+        if not message.channel: # self explanatory
             return
-        if message.author == self.bot.user:
+        if message.author == self.bot.user: # self explanatory
             return
-        if re.match(message, self._link_regex):
+        if re.match(message, self._link_regex): # self explanatory do i have to tell you everything?
             try:
                 await message.delete()
-                await message.channel.send(f'{message.author.mention}, lol i\'m annoying right? yes that\'s my purpose you dumbass')
+                await message.channel.send(f'{message.author.mention}, lol i\'m annoying right? yes that\'s my purpose you dumbass') # annoys the person even more
             except:
                 pass
 
