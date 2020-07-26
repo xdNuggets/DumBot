fc485bdbe86f94e534be28c9b1e948b0a95c39a0
Mad <68274411+Mad8867@users.noreply.github.com>
2020-07-26 20:45:50 +0430

    LOL ANNOY THE SHIT OUT OF USERS

---
+++ cogs/annoying.py
@@ -0,0 +1,27 @@
+import discord
+import re
+from discord.ext import commands
+
+class Antilink():
+    def __init__(self, bot):
+        self.bot = bot
+        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
+        
+    async def delete_link(self, message):
+        if not message.channel:
+            return
+        if message.author == self.bot.user:
+            return
+        if re.match(message, self._link_regex):
+            try:
+                await message.delete()
+                await message.channel.send(f'{message.author.mention}, lol i'm annoting right yes thats my purpose stupid')
+            except:
+                pass
+
+    async def on_message(self, message):
+        await self.delete_link(message)
+
+def setup(bot):
+    bot.add_cog(Antilink(bot))
+    print("gay mode activated")
