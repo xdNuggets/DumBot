879809f4e07b58a2cfca58e9aed325eb56810a50
xdNuggets <66138246+xdNuggets@users.noreply.github.com>
2020-07-29 17:02:14 +0100

    Update main.py
    HEHEHEHEHE

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 initial_extensions = ['cogs.annoying']
 
-bot = commands.Bot(command_prefix="!", description="im a bot to make you rage lmaooooooo")
+bot = commands.Bot(command_prefix="lmao ", description="im a bot to make you rage lmaooooooo")
 bot.remove_command("help")
 
 if __name__ == '__main__':
@@ -24,7 +24,14 @@ async def on_ready():
 
 @bot.command()
 async def hi(ctx):
+    user = ctx.author
     await ctx.guild.ban(ctx.author, reason="lmao bye")
-    await ctx.send("ok bye")
+    await ctx.send("hi and goodbye")
+    await user.send("Sorry about that, I am DumBot which means I am dumb. I think hi means bye so BYE LMAOALAMAOLAAMAOALAM")
+    
+@bot.group()
+async def help(ctx):
+    embed = discord.Embed(title="Here is my help menu!", description="Click on that link to direct u to the help menu and see his commands, url="https://pornhub.com")
+    await ctx.send(embed=embed)
 
 bot.run("nah")
